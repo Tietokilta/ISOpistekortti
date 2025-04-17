519c084fd90ca409db9b06fbbbd0c6dc60975781
otsomikkila <143948983+otsomikkila@users.noreply.github.com>
2025-04-17 17:03:01 +0300

    Login frontend (#6)
    * initial commit
    
    * Added login system which stores an auth token in user's cookies on successful login. Auth checking middleware to check for existence of auth token in user's cookies with the request. Updated package.json npm scripts and readme to make installing the dev environment easier. Nix flake for building the project with 'nix build' (#4)
    
    * feat: login form send post request
    
    ---------
    
    Co-authored-by: langsjo <104687438+langsjo@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,32 @@
 import { useState, useEffect } from 'react'
 import { Card }  from './components/Card';
+import { Login } from './components/Login';
 import taskService from './services/tasks'
+
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const [login, setLogin] = useState(true)
 
+  
   useEffect(() => {
     taskService.getAll().then(task =>
       setTasks( task )
     )
   }, [])
+  
 
+  if(login) {
+    return (
+      <div
+        className='flex flex-col items-center justify-center min-h-2/3 py-8'
+      >
+        <h1 className="text-3xl font-bold mb-4">ISOpistekortti 🤯💯</h1>
+        <Login/>
+      </div>
+    )
+  }
+  else {
 
   return (
     <div
@@ -27,7 +43,7 @@ function App() {
         )}
       </div>
     </div>
-  )
-}
+  )}
+  }
 
 export default App
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+import loginService from '../services/login'
+
+const Button = ({ name, state, setState }) => {
+    return (
+        <button className="mr-4 mt-4 bg-blue-400 hover:bg-blue-500 border rounded-2xl padding-2 p-1"
+            onClick={() => {setState(!state)}}>{name}</button>
+    )
+}
+
+export function Login() {
+    const [login, setLogin] = useState(false)
+    const [register, setRegister] = useState(false)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value)
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const handleLogin = () => {
+        console.log(username)
+        console.log(password)
+        loginService.loginPost({ username: username, password: password })
+        //make a notification
+    }
+
+    if (!login && !register) {
+        return (
+            <div className="p-6 rounded-2xl shadow-lg w-80 bg-white mb-4">
+                <h1 className="flex items-center justify-center font-bold">Kirjaudu sisään tai luo käyttäjä</h1>
+                <h1 className="flex items-center justify-center font-bold">Login or create a user</h1>
+                
+                <div className="flex items-center justify-center mt-6">
+                    <Button name='Login' state={login} setState={setLogin}/>
+                    <Button name='Create user' state={register} setState={setRegister}/>
+                </div>
+            </div>
+        )
+    }
+    else if (login) {
+        return (
+            <div className="p-6 rounded-2xl shadow-lg w-80 bg-white ">
+                <h1 className='flex items-center justify-center font-bold'>LOGIN</h1>
+                <div  className='flex items-center justify-center'>
+                    <div>
+                        <p className='font-mono'>Username</p>
+                        <input className="border" onChange={handleUsernameChange}></input>
+                        <p className='font-mono'>Password</p>
+                        <input className="border" type='password' onChange={handlePasswordChange}></input>
+                    </div>
+                </div>
+                <div className='flex items-center justify-center'>
+                    <Button name='Back' state={login} setState={setLogin}/>
+                    <button className="mr-4 mt-4 bg-blue-400 hover:bg-blue-500 border rounded-2xl padding-2 p-1"
+                        onClick={() => handleLogin()}>Ok</button>
+                </div>
+            </div>
+        )
+    }
+    else if (register) {
+        return (
+            <div className="p-6 rounded-2xl shadow-lg w-80 bg-white mb-4">
+                <h1 className="flex items-center justify-center font-bold">Create user</h1>
+                <div className='flex items-center justify-center'>
+                    <div>
+                        <p className='font-mono'>Name</p>
+                        <input className="border"></input>
+                        <p className='font-mono'>Username</p>
+                        <input className="border" type='password'></input>
+                        <p className='font-mono'>Password</p>
+                        <input className="border" type='password'></input>
+                        <p className='font-mono'>Password again</p>
+                        <input className="border" type='password'></input>
+                    </div>
+                </div>
+                <div className='flex items-center justify-center'>
+                    <Button name='Back' state={register} setState={setRegister}/>
+                    <Button name='Ok'/>
+                </div>
+            </div>
+        )
+    }
+}
+
+/*
+
+
+*/ 
