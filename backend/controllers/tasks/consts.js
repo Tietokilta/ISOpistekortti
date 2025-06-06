0e25b0bcac0bf5f67bbc45142eff915c768d3e4b
langsjo <104687438+langsjo@users.noreply.github.com>
2025-06-06 13:44:13 +0300

    feat: add /api/tasks/task_user GET endpoint which returns user's tasks and their statuses

---
+++ backend/controllers/tasks/consts.js
@@ -0,0 +1,10 @@
+const TASK_STATUS = {
+  NOT_DONE: "not_done",
+  DONE: "done",
+  REQUESTING: "requesting",
+  REJECTED: "rejected",
+}
+
+module.exports = {
+  TASK_STATUS
+}
---
+++ backend/controllers/tasks/task_user.js
@@ -0,0 +1,30 @@
+const tasksRouter = require('./tasks');
+const pool = require("../../db");
+const consts = require("./consts");
+
+// Check if `status` is valid
+function isValidStatus(status) {
+  return Object.values(consts.TASK_STATUS).includes(status);
+}
+
+tasksRouter.get('/task_user', async (request, response) => {
+  const user_id = request.user.id;
+
+  try {
+    const result = await pool.query(`
+      SELECT t.id, tu.user_id, t.title, t.description, tu.status, t.needs_admin_approval
+      FROM tasks t
+      INNER JOIN task_user tu
+        ON t.id = tu.task_id
+      WHERE tu.user_id = $1;
+    `, [user_id]);
+
+    return response.status(200).json(result.rows);
+  } catch (err) {
+    console.log("Error: " + err);
+    return response.status(500).json({
+      error: 'An unexpected error occured',
+    });
+  }
+});
+
