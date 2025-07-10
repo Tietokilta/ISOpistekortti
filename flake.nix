f0c9b9b1911a583f39cae4454aae3e71ce78f6b4
langsjo <104687438+langsjo@users.noreply.github.com>
2025-07-10 07:39:16 +0300

    chore(nix): moved flake from backend/ to project root

--- flake.nix
+++ flake.nix
@@ -18,11 +18,11 @@
     {
       packages = forEachSystem (system: let
           pkgs = nixpkgs.legacyPackages.${system};
-          default = pkgs.buildNpmPackage rec {
+          backend = (pkgs.buildNpmPackage (finalAttrs: {
             pname = "ISOpistekortti";
             version = self.rev or "dirty";
 
-            src = ./.;
+            src = ./backend;
             npmDepsHash = "sha256-w/UQarkFpAdALg/xRQktmsYBM5te/wZxBWM4vdmu7Bc=";
 
             nativeBuildInputs = with pkgs; [ 
@@ -46,20 +46,20 @@
               mkdir $out/bin
               cp -r . $out/
 
-              makeWrapper "${pkgs.nodejs}/bin/node" "$out/bin/${pname}" \
+              makeWrapper "${pkgs.nodejs}/bin/node" "$out/bin/${finalAttrs.pname}" \
                 --add-flags "$out/index.js"
 
               runHook postInstall
             '';
-          };
+          }));
 
         in {
-          inherit default;
-          docker = pkgs.dockerTools.buildLayeredImage {
-            name = default.pname;
+          inherit backend;
+          docker = pkgs.dockerTools.buildImage {
+            name = backend.pname;
             tag = "latest";
 
-            config.Cmd = ["${default}/bin/${default.pname}"];
+            config.Cmd = ["${backend}/bin/${backend.pname}"];
           };
 
           devenv-up = self.devShells.${system}.default.config.procfileScript;
@@ -84,14 +84,15 @@
 
                 {
                   processes = {
-                    postgres.exec = "npm run db:start";
-                    backend.exec = "npm run dev";
+                    postgres.exec = "cd backend && npm run db:start";
+                    backend.exec = "cd backend && npm run dev";
+                    frontend.exec = "cd frontend && npm run dev";
                   };
 
                   process.managers.process-compose.settings.processes = {
                     postgres = {
                       readiness_probe = {
-                        exec.command = "docker compose exec postgres pg_isready -d ${builtins.getEnv "DB_DATABASE"} -U ${builtins.getEnv "DB_USER"}";
+                        exec.command = "cd backend && docker compose exec postgres pg_isready -d ${builtins.getEnv "DB_DATABASE"} -U ${builtins.getEnv "DB_USER"}";
                         initial_delay_seconds = 3;
                         failure_thresholds = 3;
                       };
