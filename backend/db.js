cd987e377188b87efa7fd46008412f8a607a56a7
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-08-13 22:00:08 +0300

    fix(backend): try other fix for DB SSL

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,16 @@
 const { Pool } = require("pg");
 
-const ssl = process.env.NODE_ENV === "production";
+const useSSL = process.env.NODE_ENV === "production";
 
-console.log(`using SSL for DB: ${ssl}`);
+console.log(`using SSL for DB: ${useSSL}`);
 
 const pool = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_URL,
-  ssl,
+
+  ssl: useSSL ? { rejectUnauthorized: true } : undefined,
   port: 5432,
 });
 
