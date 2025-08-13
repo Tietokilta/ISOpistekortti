f63168c9f1b3fcea383fe40ad1786775dfbe2384
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-08-13 21:50:57 +0300

    chore(backend): add logging for db ssl

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,15 @@
 const { Pool } = require("pg");
 
+const ssl = process.env.NODE_ENV === "production";
+
+console.log(`using SSL for DB: ${ssl}`);
+
 const pool = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_URL,
-  ssl: process.env.NODE_ENV === "production",
+  ssl,
   port: 5432,
 });
 
