9540bf4df8be904ee982496eb654602602166a70
langsjo <104687438+langsjo@users.noreply.github.com>
2025-08-14 00:53:15 +0300

    feat: switch flake from devenv to basic nix

--- flake.nix
+++ flake.nix
@@ -1,33 +1,25 @@
 {
-  inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
     {
-      self,
       nixpkgs,
-      devenv,
-      systems,
       ...
     }@inputs:
     let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+      lib = nixpkgs.lib;
+      forAllSystems = lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
     in
     {
-      packages = forEachSystem (
+      packages = forAllSystems (
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          lib = pkgs.lib;
         in
         rec {
           isopistekortti = pkgs.callPackage ./package.nix { };
@@ -37,52 +29,24 @@
             contents = [ pkgs.cacert ];
             config.Cmd = [ "${lib.getExe isopistekortti}" ];
           };
-
-          devenv-up = self.devShells.${system}.default.config.procfileScript;
-          devenv-test = self.devShells.${system}.default.config.test;
         }
       );
 
-      devShells = forEachSystem (
+      devShell = forAllSystems (
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
-        {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              {
-                languages.javascript = {
-                  enable = true;
-                  npm.enable = true;
-                };
-              }
+        pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            nodejs
 
-              {
-                processes = {
-                  postgres.exec = "cd backend && npm run db:start";
-                  backend.exec = "cd backend && npm run dev";
-                  frontend.exec = "cd frontend && npm run dev";
-                };
-
-                process.managers.process-compose.settings.processes = {
-                  postgres = {
-                    readiness_probe = {
-                      exec.command = "cd backend && docker compose exec postgres pg_isready -d ${builtins.getEnv "DB_DATABASE"} -U ${builtins.getEnv "DB_USER"}";
-                      initial_delay_seconds = 3;
-                      failure_thresholds = 3;
-                    };
-                  };
-
-                  backend = {
-                    depends_on.postgres.condition = "process_healthy";
-                  };
-                };
-              }
-            ];
-          };
+            # Needed to install pg-native
+            python3
+            libpq.pg_config
+          ];
         }
       );
     };
+
 }
