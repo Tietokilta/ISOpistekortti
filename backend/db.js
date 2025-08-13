77be5e46979feb6e8a1695f0e574bd4e2b8bbcab
langsjo <104687438+langsjo@users.noreply.github.com>
2025-08-13 15:52:16 +0300

    fix: use DB_URL env var in connecting to database

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  host: "localhost",
+  host: process.env.DB_URL,
   port: 5432,
 });
 
