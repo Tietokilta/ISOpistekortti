1f0c7e6b3cae671276e6a77ab90e88e7f1cc87b6
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-08-13 22:52:48 +0300

    fix(backend): revert postgres SSL configuration, try and use PGSSLMODE env var instead

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,10 @@
 const { Pool } = require("pg");
 
-const useSSL = process.env.NODE_ENV === "production";
-
-console.log(`using SSL for DB: ${useSSL}`);
-
 const pool = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_URL,
-  ssl: useSSL ? "require" : undefined,
   port: 5432,
 });
 
